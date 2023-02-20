870aeb77d9abf173cc180ab0d2c9d23f7bcc0f13
stolenh0ff <alonsso008@gmail.com>
2023-02-20 20:22:50 -0300

    Minor Changes

--- ohmyz.sh
+++ ohmyz.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+
 sudo apt-get update && sudo apt-get upgrade -y
 
-sudo apt install zsh wget -y
+sudo apt-get install -y zsh wget
 
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+chsh -s $(which zsh)
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
