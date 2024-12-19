8a78102944d6b396c31140a2d0a72d2e0100c1a8
jackcooper <jackcooper@BT-7274>
2024-12-19 14:08:19 -0300

    Add installation script for penetration testing tools on Debian

---
+++ bash/tools.sh
@@ -0,0 +1,7 @@
+##This script will install pentest tool in a debian based distro
+
+sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
+
+sudo apt install -y nmap sqlmap john hashcat rtl-433 forensics-full openvpn lynis bettercap wireshark httrack
+
+##Install Burp Suite Community
