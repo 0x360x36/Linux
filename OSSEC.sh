fad93db1931f6fce6a42b9e736b5884316d8a1dd
jackcooper <jackcooper@BT-7274>
2024-12-19 14:08:14 -0300

    removed

--- OSSEC.sh
+++
@@ -1,4 +0,0 @@
-apt-get install build-essential make zlib1g-dev libpcre2-dev libevent-dev libssl-dev
-wget -q -O - https://updates.atomicorp.com/installers/atomic | sudo bash
-apt-get update
-apt-get install ossec-hids-server
